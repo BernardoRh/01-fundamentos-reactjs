aac4589e9f6aca37cfc2ae9e890d36d07c20a57a
Bernardo Rodrigo Horbach <bernardo.horbach@gmail.com>
2022-11-16 17:07:42 -0300

    Making the iteration of Posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,112 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css'
 import './global.css'
 
-function App() {
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/BernardoRh.png',
+      name: 'Bernardo R. Horbach',
+      role: 'Web Developer',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋', 
+      },
 
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      
+      {
+        type: 'link',
+        content: '👉 jane.design/doctorcare',
+      },
+
+      {
+        type: 'citacions',
+        content: [
+          {
+            type: 'link',
+            content: '#novoprojeto',
+          },
+          {
+            type: 'link',
+            content: '#nlw',
+          },
+          {
+            type: 'link',
+            content: '#rocketseat',
+          },
+        ]
+      }
+    ],
+    publishedAt: new Date('2022-11-06 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Myke Brito',
+      role: 'Educator @Rockeatseat',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋', 
+      },
+
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      
+      {
+        type: 'link',
+        content: '👉 jane.design/doctorcare',
+      },
+
+      {
+        type: 'citacions',
+        content: [
+          {
+            type: 'link',
+            content: '#novoprojeto',
+          },
+          {
+            type: 'link',
+            content: '#nlw',
+          },
+          {
+            type: 'link',
+            content: '#rocketseat',
+          },
+        ]
+      }
+    ],
+    publishedAt: new Date('2022-11-16 10:00:00'),
+  }
+]
+
+
+function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-          author="Bernardo R. Horbach"
-          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, doloribus veniam. Quidem reiciendis voluptas totam similique doloribus quae, omnis aliquid cupiditate architecto quis nisi non sequi debitis sapiente id et."
-          />
-          <Post
-          author="Diego Fernandes"
-          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, doloribus veniam. Quidem reiciendis voluptas totam similique doloribus quae, omnis aliquid cupiditate architecto quis nisi non sequi debitis sapiente id et."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,20 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import styles from './Post.module.css';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
-export function Post(){
+export function Post({author, publishedAt, content}){
+   const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+      locale: ptBR,
+   });
+
+   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+      locale: ptBR,
+      addSuffix: true,
+   })
+
    return(
       <article className={styles.post}>
          <img
@@ -11,28 +23,56 @@ export function Post(){
          />
          <header>
             <div className={styles.author}>
-               <Avatar src="https://github.com/BernardoRh.png" />
+               <Avatar src={author.avatarUrl} />
                <div className={styles.authorInfo}>
-                  <strong>Bernardo R Horbach</strong>
-                  <span>Web Developer</span>
+                  <strong>{author.name}</strong>
+                  <span>{author.role}</span>
                </div>
             </div>
 
-            <time title='14 de Novembro as 12:45h' dateTime='2022-11-14 12:44:00'>Públicado há 1h</time>
+            <time title={publishedDateFormated} dateTime={publishedAt.toISOString()}>
+               {publishedDateRelativeToNow}
+            </time>
          </header>
          
          <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
-
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-            <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
-
-            <p>
-               <a href="#">#novoprojeto</a>{' '}
-               <a href="#">#nlw</a>{' '}
-               <a href="#">#rocketseat</a>
-            </p>
+            {content.map(line => {
+               if(line.type == 'paragraph') {
+                  return (
+                     <p>
+                        {line.content}
+                     </p>
+                  );
+               } else if(line.type == 'link') {
+                  return(
+                     <p>
+                        <a href='#'>
+                           {line.content}
+                        </a>
+                     </p>
+                  );
+               } else if(line.type == 'citacions') {
+                  return(
+                     <p>{
+                        line.content.map(citation => {
+                           if(citation.type == 'paragraph') {
+                              return (
+                                 <>
+                                    {citation.content + ' '}
+                                 </>
+                              );
+                           } else if(citation.type == 'link') {
+                              return(
+                                 <a href='#'>
+                                    {citation.content + ' '}
+                                 </a>
+                              );
+                           }
+                        })
+                     }</p>
+                  );
+               }
+            })}
          </div>
 
          <form className={styles.commentForm}>
